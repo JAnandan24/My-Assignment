e0e7be58668d1f2bd55b12c29333810b047d45a7
JAnandan24 <jayabalan.anandan11@gmail.com>
2024-11-30 14:11:33 +0530

    var vs let vs const

---
+++ 02 var vs let vs const.js
@@ -0,0 +1,15 @@
+const BrowserVersion ='Chrome';
+
+function getbrowserversion()
+{
+if (BrowserVersion.startsWith('Chrome'))
+     { let BrowserVersion ="Chrome 131";  // var BrowserVersion declare undefined 
+        
+        console.log("The Browser version is " + BrowserVersion);}
+        
+        else {
+    console.log("Firefox");}
+}
+
+getbrowserversion();
+
