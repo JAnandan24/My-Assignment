de7153275a3ef4e323ae967de43037285452acd8
JAnandan24 <jayabalan.anandan11@gmail.com>
2024-11-30 12:20:01 +0530

    Assignment 01

---
+++ 01 Primitive Data Types.js
@@ -0,0 +1,26 @@
+let Name = `J Anandan`;
+
+let CompanyName = "Testleaf24";
+
+let MobileNumber = 9876543210;
+
+let IsAutomation = true;
+
+let HasPlaywright;
+
+
+console.log(`Name = ` + Name);
+console.log(`Data Type = ` + typeof Name);
+
+console.log(`CompanyName = ` + CompanyName);
+console.log(`Data Type = ` + typeof CompanyName);
+
+console.log(`MobileNumber = ` + MobileNumber);
+console.log(`Data Type = ` + typeof MobileNumber);
+
+console.log(`IsAutomation = ` + IsAutomation);
+console.log(`Data Type = ` + typeof IsAutomation);
+
+console.log(`HasPlaywright = ` + HasPlaywright);
+console.log(`Data Type = ` + typeof HasPlaywright);
+
